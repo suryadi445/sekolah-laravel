13c44ef984ee9f3208f9b4f144072150ca98cfe2
suryadi445 <suryadi.hhb@gmail.com>
2023-02-27 06:44:11 +0700

    create crud for master jabatan

---
+++ app/Http/Controllers/JabatanController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Jabatan;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class JabatanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $jabatan = Jabatan::latest()->paginate(20);
+
+        return view('backend.jabatan', compact(['jabatan']));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'kode_jabatan' => 'string',
+            'nama_jabatan' => 'required',
+        ]);
+
+        $insert = Jabatan::create([
+            'kode_jabatan' => $request->kode_jabatan,
+            'nama_jabatan' => $request->nama_jabatan,
+            'user' => Auth::id(),
+        ]);
+
+        if ($insert) {
+            return back()->with('success', 'Success! Data saved successfully');
+        } else {
+            return back()->with('failed', 'Alert! Data failed to save');
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Jabatan  $jabatan
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Jabatan $jabatan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Jabatan  $jabatan
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Jabatan $jabatan)
+    {
+        $data = Jabatan::find($jabatan->id);
+
+        return response()->json($data);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Jabatan  $jabatan
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Jabatan $jabatan)
+    {
+        $request->validate([
+            'kode_jabatan' => 'string',
+            'nama_jabatan' => 'required',
+        ]);
+
+        $update = Jabatan::where('id', $request->id)
+            ->update([
+                'kode_jabatan' => $request->kode_jabatan,
+                'nama_jabatan' => $request->nama_jabatan,
+                'user' => Auth::id(),
+            ]);
+
+        if ($update) {
+            return back()->with('success', 'Success! Data saved successfully');
+        } else {
+            return back()->with('failed', 'Alert! Data failed to save');
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Jabatan  $jabatan
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Jabatan $jabatan)
+    {
+        $delete = Jabatan::destroy($jabatan->id);
+
+        if ($delete) {
+            return back()->with('success', 'Success! Data successfuly deleted');
+        } else {
+            return back()->with('failed', 'Alert! Data failed to deleted');
+        }
+    }
+}
---
+++ resources/views/backend/jabatan.blade.php
@@ -0,0 +1,192 @@
+@extends('layouts.admin.admin')
+
+@section('admin')
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="row">
+                                <div class="offset-md-9 col-md-3">
+                                    <button type="button" class="btn btn-primary float-end" data-bs-toggle="modal"
+                                        data-bs-target="#modal_add">
+                                        <i class="fa-solid fa-plus"></i>
+                                        Tambah Data
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="table-responsive">
+                                <table
+                                    class="table table-striped text-center text-capitalize table-responsive rounded rounded-1 overflow-hidden">
+                                    <thead class="bg-dark text-light">
+                                        <thead class="bg-dark text-light">
+                                            <tr>
+                                                <th scope="col">Jabatan</th>
+                                                <th scope="col">Kode Jabatan</th>
+                                                <th scope="col">User</th>
+                                                <th scope="col">Action</th>
+                                            </tr>
+                                        </thead>
+                                    <tbody>
+                                        @if (!empty($jabatan))
+                                            @foreach ($jabatan as $item)
+                                                <tr>
+                                                    <td>{{ $item->nama_jabatan }}</td>
+                                                    <td>{{ $item->kode_jabatan }}</td>
+                                                    <td>{{ getUser($item->user)->name ?? '' }}</td>
+                                                    <td>
+                                                        <button type="button" class="btn btn-sm btn-warning btn_edit"
+                                                            data-id="{{ $item->id }}" data-bs-toggle="modal"
+                                                            data-bs-target="#modal_edit">
+                                                            <i class="fa-solid fa-pen-to-square"></i>
+                                                            Edit
+                                                        </button>
+                                                        <form method="POST"
+                                                            action="{{ route('jabatan.destroy', $item->id) }}">
+                                                            @method('DELETE')
+                                                            @csrf
+                                                            <button type="submit"
+                                                                class="btn btn-sm btn-danger btn_delete mt-2">
+                                                                <i class="fa-solid fa-trash"></i>
+                                                                Delete
+                                                            </button>
+                                                        </form>
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+                                        @endif
+
+
+                                        @if ($jabatan->count() == 0)
+                                            <td colspan="7">
+                                                <span class="text-danger">Data Tidak Tersedia </span>
+                                            </td>
+                                        @endif
+                                    </tbody>
+                                </table>
+
+                                <div class="d-flex justify-content-center">
+                                    {!! $jabatan->links() !!}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <!-- Modal -->
+    <div class="modal fade" id="modal_add" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Tambah Data</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <form action="{{ route('jabatan.store') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <div class="modal-body">
+                        <div class="form-floating mt-3">
+                            <input type="text" class="form-control" placeholder="Leave a comment here"
+                                name="kode_jabatan" value="{{ old('kode_jabatan') }}">
+                            <label for="kode_jabatan">Kode Jabatan</label>
+                        </div>
+                        <div class="form-floating mt-3">
+                            <input type="text" class="form-control" placeholder="Leave a comment here"
+                                name="nama_jabatan" value="{{ old('nama_jabatan') }}">
+                            <label for="nama_jabatan">Nama Jabatan</label>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Simpan</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="modal_edit" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Edit Data</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <form action="" method="POST" enctype="multipart/form-data" id="form_edit">
+                    @csrf
+                    @method('PUT')
+                    <div class="modal-body">
+                        <input type="hidden" id="id" name="id">
+                        <div class="form-floating mt-3">
+                            <input type="text" class="form-control" placeholder="Leave a comment here"
+                                id="kode_jabatan" name="kode_jabatan">
+                            <label for="kode_jabatan">Kode Jabatan</label>
+                        </div>
+                        <div class="form-floating mt-3">
+                            <input type="text" class="form-control" placeholder="Leave a comment here"
+                                id="nama_jabatan" name="nama_jabatan">
+                            <label for="nama_jabatan">Nama Jabatan</label>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Simpan</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        $(function() {
+            $(document).on('click', '.hapus_gambar', function(e) {
+                e.preventDefault()
+                url = $(this).attr('href')
+                window.location.href = url;
+            })
+
+            $(document).on('click', '.btn_edit', function() {
+                var id = $(this).attr('data-id')
+                $.get("{{ route('jabatan.index') }}" + '/' + id + '/edit', function(data) {
+                    $('#id').val(id);
+                    $('#kode_jabatan').val(data.kode_jabatan);
+                    $('#nama_jabatan').val(data.nama_jabatan);
+                    $('#form_edit').attr('action', "{{ route('jabatan.index') }}" + '/' + id);
+
+                })
+            })
+
+            $(document).on('click', '.btn_delete', function(e) {
+                e.preventDefault()
+                var form = $(this).closest("form");
+
+                Swal.fire({
+                    title: 'Are you sure?',
+                    text: "Data Tidak bisa dikembalikan!",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#d33',
+                    cancelButtonColor: '#6e7881',
+                    confirmButtonText: 'Yes, delete it!'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+
+                        form.submit();
+
+                    }
+                })
+            })
+        });
+    </script>
+@endsection
